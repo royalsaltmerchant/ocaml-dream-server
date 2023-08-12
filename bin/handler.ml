6f8addc8449c6c530f4565c5cea8fcd96ba51b53
Julian <jumpingafterrain@gmail.com>
2023-08-12 14:13:18 +0200

    user set id not email " ;

--- bin/handler.ml
+++ bin/handler.ml
@@ -14,9 +14,9 @@ let handle_login req =
     print_endline input_email; print_endline input_password;
     match%lwt Dream.sql req (get_user_by_email_query input_email) with
     | [] -> Dream.empty `Bad_Request
-    | ((_id, email, password) :: _) -> (
+    | ((id, _email, password) :: _) -> (
       if verify input_password (hash_of_string password) then
-        (let%lwt () = Dream.set_session_field req "user" email in
+        (let%lwt () = Dream.set_session_field req "user" (string_of_int id) in
         Dream.redirect req "/todos")
       else Dream.empty `Bad_Request))
   | _ -> Dream.empty `Bad_Request
@@ -29,7 +29,7 @@ let echo req =
 let todos req =
   match Dream.session_field req "user" with
   | None -> Dream.empty `Forbidden
-  | Some _email -> (
+  | Some _user -> (
     let* todos = Dream.sql req get_todos_query in
     let todo_to_object (id, description, is_done) =
       Jg_types.Tobj [
